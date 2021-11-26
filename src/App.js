c89cd93689d6e40533b945f4342cd520e01e5d69
mae616 <oya2.mi@hotmail.co.jp>
2021-11-26 21:14:40 +0900

    一直線に上に進む

--- src/App.js
+++ src/App.js
@@ -1,12 +1,63 @@
+import { useEffect, useState } from 'react';
 import Navigation from './components/Navigation';
 import Field from './components/Field';
 import Button from './components/Button';
 import ManipulationPanel from './components/ManipulationPanel';
 import { initFields } from './utils'
 
-const fields = initFields(35)
+const initialPosition = { x: 17, y: 17 };
+const initialValues = initFields(35, initialPosition);
+const defaultInterval = 100;
+
+// let timer = undefined;
+let timer = null;
+
+const unsubscribe = () => {
+  if (!timer) {
+    return
+  }
+  clearInterval(timer);
+}
 
 function App() {
+  const [fields, setFields] = useState(initialValues);
+  const [position, setPosition] = useState(initialPosition);
+  const [tick, setTick] = useState(0);
+
+  useEffect(() => {
+    // setPosition(initialPosition);
+    timer = setInterval(() => {
+      // if (!position) { // useEffectの場合の考慮
+      //   return;
+      // }
+      // console.log(position)
+      // goUp();
+      setTick(tick => tick + 1);
+    }, defaultInterval);
+
+    // useEffectのリターンにするとコンポーネントが削除されるタイミングで実行される
+    return unsubscribe;
+  }, []);
+
+  useEffect(() => {
+    if (!position) {
+      return;
+    }
+    goUp();
+  }, [tick]);
+
+  const goUp = () => {
+    const { x, y } = position;
+    const nextY = Math.max(y - 1, 0);
+    const nextFields = fields.map(field => [...field]);
+    nextFields[y][x] = '';
+    nextFields[nextY][x] = 'snake';
+    setPosition({ x, y: nextY });
+    // setFields(fields);
+    setFields(nextFields);
+
+  }
+
   return (
     <div className="App">
       <header className="header">
--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -2,7 +2,6 @@ const Field = ({ fields }) => {
     return (
         <div className="field">
             {
-                // new Array(35 * 35).fill('').map(() => <div className="dots"></div>)
                 fields.map((row) => {
                     return row.map((column) => {
                         return <div className={`dots ${column}`}></div>
