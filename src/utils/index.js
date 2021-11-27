7ddccada70fc2bd43597ffbb496997f03f46d311
mae616 <oya2.mi@hotmail.co.jp>
2021-11-27 20:23:16 +0900

    餌の配置

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,27 @@
-export const initFields = (fieldSize, initialPosition) => {
+const getFoodPosition = (fieldSize, excludes) => {
+    while (true) {
+        const x = Math.floor(Math.random() * (fieldSize - 1 - 1)) + 1;
+        const y = Math.floor(Math.random() * (fieldSize - 1 - 1)) + 1;
+        // 蛇の体の座標 配列と一致するとこには餌を置かない
+        const conflict = excludes.some(item => item.x === x && item.y === y);
+
+        if (!conflict) {
+            return { x, y };
+        }
+    }
+}
+
+
+export const initFields = (fieldSize, snake) => {
     const fields = [];
     for (let i = 0; i < fieldSize; i++) {
         const cols = new Array(fieldSize).fill('');
         fields.push(cols);
     }
-    fields[initialPosition.x][initialPosition.y] = 'snake'
+    fields[snake.x][snake.y] = 'snake'
+
+    const food = getFoodPosition(fieldSize, [snake]);
+    fields[food.y][food.x] = 'food';
 
     return fields;
 }
