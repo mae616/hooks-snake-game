bfebcc347dc2b13022e67ba244e4b6c3e2528f46
mae616 <oya2.mi@hotmail.co.jp>
2021-11-28 21:13:44 +0900

    スタイルの適用

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,10 +3,10 @@ import { GameStatus } from "../App";
 const Button = ({ status, onStart, onStop, onRestart }) => {
     return (
         <div className="button">
-            {status === GameStatus.gameover && <button onClick={onRestart}>gameover</button>}
-            {status === GameStatus.init && <button onClick={onStart}>start</button>}
-            {status === GameStatus.suspended && <button onClick={onStart}>start</button>}
-            {status === GameStatus.playing && <button onClick={onStop}>stop</button>}
+            {status === GameStatus.gameover && <button className="btn btn-gameover" onClick={onRestart}>gameover</button>}
+            {status === GameStatus.init && <button className="btn btn-init" onClick={onStart}>start</button>}
+            {status === GameStatus.suspended && <button className="btn btn-suspended" onClick={onStart}>start</button>}
+            {status === GameStatus.playing && <button className="btn btn-playing" onClick={onStop}>stop</button>}
         </div>
     );
 };
